aea68f8215db18b2f8ece8aae5ca606db4ac5918
Tobias Duggan <tobias.duggan@protonmail.com>
2020-07-08 22:10:39 +0100

    Add some personalisation (website title and images)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `tobiasDuggan`,
+    description: `A place to discuss all of the personal projects I plan to undertake.`,
+    author: `Tobias Duggan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from "../images/logo_white.png"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -13,7 +14,7 @@ const Header = ({ siteTitle }) => (
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1.45rem 1rem 0.2rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -24,7 +25,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+           <img src={logo} alt="" height={100} />
         </Link>
       </h1>
     </div>
