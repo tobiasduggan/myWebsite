e11ff7fd0816ab65fd1a419b522fa0011f1dcb2f
Tobias Duggan <tobias.duggan@protonmail.com>
2020-07-08 23:01:20 +0100

    add bg image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,27 @@
 import React from "react"
-import { Link } from "gatsby"
+
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import BackgroundImage from "../images/codeBG.jpg"
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Welcome</h1>
+    <div style ={{
+      backgroundImage: `url(${BackgroundImage})`,
+      backgroundSize: "cover",
+      height: "100vh"}}
+      >
+      <h1>Welcome</h1>
+    </div>
     <p>Welcome to my new website!</p>
     <p>Now, have a nice day.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image/>
     </div>
-    {/* <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
