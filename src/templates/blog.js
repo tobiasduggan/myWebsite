f65a3b263eabfd3eec4c4e0e0efb189ee1973c4d
Tobias Duggan <tobias.duggan@protonmail.com>
2020-07-10 15:47:44 +0100

    add formatting layout to blog posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
+import Layout from "../components/layout"
 
 export default function Template({data}) {
     const {markdownRemark: post} = data;
     return (
-        <div>
+        <Layout>
+          <div>
             <h1>{post.frontmatter.title}</h1>
             <div dangerouslySetInnerHTML={{__html: post.html}}/>
-        </div>
+          </div>
+        </Layout>
     )
 }
 
